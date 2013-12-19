f9d53fabaa7de566b91553551f044cd55dd20aed
Mac User <macuser@Mac-Users-iMac.local>
2013-12-19 18:43:43 +0200

    Fixed file upload

--- RISE/appAppDelegate.m
+++ RISE/appAppDelegate.m
@@ -22,7 +22,10 @@
 	
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    backend = [NSTask launchedTaskWithLaunchPath:@"/opt/rise/bin/nitrogen" arguments:[NSArray arrayWithObject:@"start"]];
+    
+    //bool r = [conf copyItemAtPath:@"/usr/local/rise/etc/app.config" toPath:@"$HOME/Library/rise" error:&err];
+    //[conf createDirectoryAtPath:@"~/Library/test" withIntermediateDirectories:YES attributes:nil error:nil];
+    backend = [NSTask launchedTaskWithLaunchPath:@"/usr/local/rise/bin/nitrogen" arguments:[NSArray arrayWithObject:@"start"]];
     [backend waitUntilExit];
     
     while ([NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://localhost:8000"]] == nil);
@@ -31,9 +34,39 @@
 }
 - (NSApplicationTerminateReply) applicationShouldTerminate:(NSApplication *)sender
 {
-    backend = [NSTask launchedTaskWithLaunchPath:@"/opt/rise/bin/nitrogen" arguments:[NSArray arrayWithObject:@"stop"]];
+    backend = [NSTask launchedTaskWithLaunchPath:@"/usr/local/rise/bin/nitrogen" arguments:[NSArray arrayWithObject:@"stop"]];
     //[backend waitUntilExit];
     return NSTerminateNow;
 }
+- (void)webView:(WebView *)sender runOpenPanelForFileButtonWithResultListener:(id < WebOpenPanelResultListener >)resultListener
+{       
+    // Create the File Open Dialog class.
+    NSOpenPanel* openDlg = [NSOpenPanel openPanel];
+    
+    // Enable the selection of files in the dialog.
+    [openDlg setCanChooseFiles:YES];
+    
+    // Enable the selection of directories in the dialog.
+    [openDlg setCanChooseDirectories:NO];
+    
+    if ( [openDlg runModal] == NSOKButton )
+    {
+        NSArray* URLs = [openDlg URLs];
+        NSMutableArray *files = [[NSMutableArray alloc]init];
+        for (int i = 0; i <[URLs count]; i++) {
+            NSString *filename = [[URLs objectAtIndex:i]relativePath];
+            [files addObject:filename];
+        }
+        
+        for(int i = 0; i < [files count]; i++ )
+        {
+            NSString* fileName = [files objectAtIndex:i];
+            [resultListener chooseFilename:fileName]; 
+        }
+        [files release];
+    }
+    
+}
+
 
 @end
